4e48b2be46761be22f66a941633476ce53d1ea69
PrincewillDev <princewillfidelis1@gmail.com>
2025-01-29 22:10:43 +0100

    Updating allowed host in settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!urw_j19##ol5suu5#b0aq#7xb7$-y+-sn3ywoumpz&@&wb(q2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["public-api-production-7b03.up.railway.app"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -134,5 +134,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-PORT = os.getenv("")
 

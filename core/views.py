6bf860250ce0626449ff570c79b5a61e56a8a0c6
PrincewillDev <princewillfidelis1@gmail.com>
2025-01-30 10:17:08 +0100

    Fixed the issue on my api response format

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from rest_framework import status
-from rest_framework.response import Response
+# from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
@@ -14,4 +15,4 @@ def intern_details(request):
         "current_datetime": datetime.now().isoformat(),
         "github_url": "https://github.com/PrincewillDev/Public-Api"
     }
-    return Response(data, status=status.HTTP_200_OK)
+    return JsonResponse(data, status=status.HTTP_200_OK)
